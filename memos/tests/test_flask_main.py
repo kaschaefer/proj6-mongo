c3b91ea00086b4aca14db5bf76f5c87943b55857
Kaela Schaefer <kaelaxxanne@gmail.com>
2017-11-07 15:35:52 -0800

    Add test

---
+++ memos/tests/test_flask_main.py
@@ -0,0 +1,12 @@
+"""
+Nose tests for flask_main.py
+"""
+from flask_main import #TODO:: Insert functions here
+
+import nose
+import logging
+import arrow
+
+logging.basicConfig(format='%(levelname)s:%(message)s',
+                    level=logging.WARNING)
+log = logging.getLogger(__name__)
